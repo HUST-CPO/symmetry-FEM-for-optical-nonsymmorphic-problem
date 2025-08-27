dc0339dd341d511a0db688056ae02e78e450ada5
Lida_Liu <112149564+2577243392@users.noreply.github.com>
2025-08-27 16:50:36 +0800

    Add files via upload

---
+++ NLS/NLS(0,v,0)+PMC/MakeMeshData.m
@@ -0,0 +1,15 @@
+clc
+clear all
+
+model=mphload('NLS_Y.mph');
+[m1, m2]=mphmeshstats(model);
+Mesh.NbrNodes=max(size(m2.vertex));%顶点数
+Mesh.NbrElements=max(size(m2.elem{2}));%四面体数
+Mesh.Nodes=m2.vertex; %顶点坐标 3*n
+Mesh.Domains=m2.elementity{2};%四面体区域索引 n*1
+Mesh.Elements=m2.elem{2}+1;%四面体顶点索引 4*n
+Mesh.Tris=m2.elem{3}+1;%边界面
+Mesh.Trisflag=m2.elementity{3};%边界面所在区域
+Mesh.Elements = sort(Mesh.Elements);
+
+save('MeshData.mat','Mesh');
